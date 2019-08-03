3008acf02a7191d23f9b08252a96284df91479d5
chaudharyachint08 <chaudharyachint08@gmail.com>
2019-08-03 19:22:30 +0530

    GCS util start

--- driveex/download.py
+++ driveex/download.py
@@ -51,10 +51,11 @@ def gd_move_to_dir(file_id,folder_id):
     # Move the file to the new folder
     file = drive_service.files().update(fileId=file_id,
                                         addParents=folder_id,
-                                        removeParents=previous_parents,
-                                        fields='id, parents').execute()
+                                        fields='id, parents',
+                                        **({'removeParents':previous_parents} if file.get('parents') else {}),
+                                        ).execute()
 
-def gd_download(drive_name,drive_path_id=None,disk_name=None,disk_path='.'):
+def gd_download(drive_name,disk_name=None,drive_path_id=None,disk_path='.'):
     if disk_name is None:
         disk_name = drive_name
     # Call the Drive v3 API
@@ -79,8 +80,18 @@ def gd_download(drive_name,drive_path_id=None,disk_name=None,disk_path='.'):
             if done:
                 break
 
-def gd_upload():
-    pass
+def gd_upload(drive_name,disk_name=None,drive_path_id=None,disk_path='.'):
+    if disk_name is None:
+        disk_name = drive_name
+
+    file_metadata = {'name': 'photo.jpg'}
+    if drive_path_id is not None:
+        file_metadata.update({'parents': [folder_id]})
+    media = MediaIoBaseUpload(os.path.join(disk_path,disk_name), resumable=True)
+    file = drive_service.files().create(body=file_metadata,
+                                        media_body=media,
+                                        fields='id').execute()
+    print('File ID: %s' % file.get('id'))
 
 
 if __name__ == '__main__':
