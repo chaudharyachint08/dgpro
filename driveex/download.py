64384f523fcc812333fde623d72f640d8626f034
chaudharyachint08 <chaudharyachint08@gmail.com>
2019-08-03 15:27:14 +0530

    GCS util start

--- driveex/download.py
+++ driveex/download.py
@@ -1,49 +1,14 @@
 from __future__ import print_function
-import pickle
+import pickle, io
 import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from googleapiclient.http import MediaIoBaseUpload,MediaIoBaseDownload
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+SCOPES = ['https://www.googleapis.com/auth/drive']
 
-def main():
-    """Shows basic usage of the Drive v3 API.
-    Prints the names and ids of the first 10 files the user has access to.
-    """
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-
-    service = build('drive', 'v3', credentials=creds)
-
-    # Call the Drive v3 API
-    results = service.files().list(
-        pageSize=10, fields="nextPageToken, files(id, name)").execute()
-    items = results.get('files', [])
-
-    if not items:
-        print('No files found.')
-    else:
-        print('Files:')
-        for item in items:
-            print(u'{0} ({1})'.format(item['name'], item['id']))
 
 def get_service():
     """Shows basic usage of the Drive v3 API.
@@ -70,3 +35,39 @@ def get_service():
 
     service = build('drive', 'v3', credentials=creds)
     return service
+
+
+def download_file(drive_name,drive_path='.',disk_name=None,disk_path='.'):
+    if disk_name is None:
+        disk_name = drive_name
+    # Call the Drive v3 API
+    service = get_service()
+    results = service.files().list(
+        pageSize=10, fields="nextPageToken, files(id, name)").execute()
+    items = results.get('files', [])
+
+    if not items:
+        print('No files found.')
+    else:
+        print('Files:')
+        for item in items:
+            print(u'{0} ({1})'.format(item['name'], item['id']))
+            if item['name']==download_name:
+                file_id = item['id']
+                file_name = item['name']
+
+    #   file_id = '1ZdR3L3qP4Bkq8noWLJHSr_iBau0DNT4Kli4SxNc2YEo'
+    request = service.files().get_media(fileId=file_id)
+    fh = io.FileIO(download_name,'wb')
+    downloader = MediaIoBaseDownload(fh, request)
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+        print("Download %d%%." % int(status.progress() * 100))
+
+
+if __name__ == '__main__':
+    disk_path,  disk_name  = '.','data.csv'
+    drive_path, drive_name = '.','data.csv'
+    gd_upload()
+    gd_download()
