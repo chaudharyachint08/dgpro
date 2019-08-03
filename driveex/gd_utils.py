23340fa83e51c348dea539e15205f0d2bf38f701
chaudharyachint08 <chaudharyachint08@gmail.com>
2019-08-03 23:15:32 +0530

    gd utils

--- driveex/gd_utils.py
+++ driveex/gd_utils.py
@@ -5,8 +5,7 @@ https://developers.google.com/drive/api/v3/search-files
 https://developers.google.com/drive/api/v3/reference/files/list
 '''
 
-import pickle, io
-import os.path
+import os, pickle, io
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -20,6 +19,7 @@ gd_name_to_id = {'.':None} # Drive Folder when Nowhere has None as ID
 
 
 def init_service():
+    'initiate global drive_service variable for GDrive access'
     global drive_service
     """Shows basic usage of the Drive v3 API.
     Prints the names and ids of the first 10 files the user has access to.
@@ -44,19 +44,15 @@ def init_service():
             pickle.dump(creds, token)
     drive_service = build('drive', 'v3', credentials=creds)
 
-
 def gd_mkdir(name):
+    'Creating a folder in My Drive for storage management'
     file_metadata = { 'name': name, 'mimeType': 'application/vnd.google-apps.folder' }
     file = drive_service.files().create(body=file_metadata,fields='id').execute()
     gd_name_to_id[name] = file.get('id')
     print('Folder Created with ID: %s' % file.get('id'))
 
-
-def gd_remove(id):
-    drive_service.files().delete(fileId=id).execute()
-
-
 def gd_move_to_dir(file_id,folder_id):
+    ''
     # Retrieve the existing parents to remove
     file = drive_service.files().get(fileId=file_id,
                                      fields='parents').execute()
@@ -68,13 +64,44 @@ def gd_move_to_dir(file_id,folder_id):
                                         **({'removeParents':previous_parents} if file.get('parents') else {}),
                                         ).execute()
 
+def gd_remove(id): 
+    'Removing a file (or folder-file) from GDrive'
+    drive_service.files().delete( fileId=id ).execute()
 
-def gd_download(drive_name,disk_name=None,drive_path_id=None,disk_path='.'):
-    if disk_name is None:
-        disk_name = drive_name
+def find_dir(dir_name):
+    'Find given directory in GDrive and if exist, set it in gd_name_to_id'
+    # Call the Drive v3 API
+    results = drive_service.files().list(q="mimeType='application/vnd.google-apps.folder'",
+    	fields="nextPageToken, files(id, name)").execute()
+    items = results.get('files', [])
+    for item in items:
+        if item['name'] == dir_name:
+            file_id, file_name = item['id'], item['name']
+            gd_name_to_id[file_name] = file_id
+            return True
+    return False
+
+def find_file(file_name,drive_path_id=None):
+    'Find given file [in directory] in GDrive and if exist, set it in gd_name_to_id'
     # Call the Drive v3 API
     results = drive_service.files().list(fields="nextPageToken, files(id, name)",
-        **({} if drive_path_id is None else {'q':(drive_path_id in 'parents')})).execute()
+    	**({} if drive_path_id is None else {'q':"%s in parents"%repr(drive_path_id)})).execute()
+    items = results.get('files', [])
+    for item in items:
+        if item['name'] == file_name:
+            file_id, file_name = item['id'], item['name']
+            gd_name_to_id[file_name] = file_id
+            return True
+    return False
+
+
+def gd_download(drive_name,disk_name=None,drive_path_id=None,disk_path='.'):
+    'Downloader from GDrive'
+    if disk_name is None:
+        disk_name = drive_name
+    results = drive_service.files().list(fields="nextPageToken, files(id, name)",
+        **({} if drive_path_id is None else {'q':"%s in parents"%repr(drive_path_id)})
+        ).execute()
     items = results.get('files', [])
     for item in items:
         print(u'{0} ({1})'.format(item['name'], item['id']))
@@ -95,8 +122,8 @@ def gd_download(drive_name,disk_name=None,drive_path_id=None,disk_path='.'):
             if done:
                 break
 
-
 def gd_upload(drive_name,disk_name=None,drive_path_id=None,disk_path='.'):
+    'Uploader for GDrive'
     if disk_name is None:
         disk_name = drive_name
 
@@ -114,10 +141,14 @@ def gd_upload(drive_name,disk_name=None,drive_path_id=None,disk_path='.'):
 
 if __name__ == '__main__':
     disk_path,  disk_name  = '.', 'data.csv'
-    drive_path, drive_name = '.', 'data.csv'
+    drive_path, drive_name = 'datasets', 'data.csv'
+
     init_service()
-    if drive_path != '.':
+
+    if (drive_path != '.') and (not find_dir(drive_path)):
         gd_mkdir(drive_path)
+    if find_file(drive_name,gd_name_to_id[drive_path]):
+	    gd_remove(gd_name_to_id[drive_name])
     gd_upload (  drive_name,disk_name,gd_name_to_id[drive_path],disk_path )
     gd_download( drive_name,disk_name,gd_name_to_id[drive_path],disk_path )
     gd_remove(gd_name_to_id[drive_name])
