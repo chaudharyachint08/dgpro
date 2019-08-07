16421a0c0ec890e5c0e1421feff82bd6f49f765d
chaudharyachint08 <chaudharyachint08@gmail.com>
2019-08-07 23:05:16 +0530

    GUIautomation_working

---
+++ ex/pyautogui/main.py
@@ -0,0 +1,9 @@
+from AutomateColab import AutomateColab
+
+if __name__=='__main__':
+    '                      directory, PAUSE, FAIL_SAFE, SLEEP_DURATION, DRAG_DELAY_LIMIT, OUT_CENTER'
+    colab = AutomateColab( 'images',    1,      True,         10,               1,            0.25 )
+    assert (colab.WIDTH,colab.HEIGHT)==(1366,768)
+    colab.start()
+    st = input('Enter string to end process')
+    colab.auto_thread_running = False
